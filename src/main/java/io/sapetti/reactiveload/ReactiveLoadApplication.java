fafe52caf298eabc9e351dc5c8a0e3735e988448
César Sapetti <gsapetti@hotmail.com>
2020-03-17 22:17:39 +0100

    Initial commit

---
+++ src/main/java/io/sapetti/reactiveload/ReactiveLoadApplication.java
@@ -0,0 +1,13 @@
+package io.sapetti.reactiveload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveLoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveLoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sapetti/reactiveload/ReactiveLoadApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sapetti.reactiveload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveLoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
